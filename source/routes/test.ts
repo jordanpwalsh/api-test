c0cb47740a084f766de1f816e3f3c904f171e7d6
Jordan Walsh <jowalsh@redhat.com>
2023-07-24 23:04:46 -0400

    work on project restructure

--- source/routes/test.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import controller from '../controllers/test';
-
-const router = express.Router();
-
-router.get("/get", controller.getTest);
-router.post("/post", controller.postTest);
-
-export = router
